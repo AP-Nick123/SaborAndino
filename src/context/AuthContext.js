ef94557d0f8054474cb0269c7cba6a22e07dfc5f
Nick <nick@gmail.com>
2025-06-06 12:43:48 -0500

    Panel Admin 2 prt

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,6 @@
 
-
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -13,3 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
