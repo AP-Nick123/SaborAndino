84e8fa5589ccf269d56ae446276053e6c1f3046c
Cesar <augustobalveton@gmail.com>
2025-06-06 12:44:23 -0500

    Creando Seccion de Reservaciones

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,16 @@
-
-
 import { initializeApp } from "firebase/app";
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyCXQkSjs1E7vA0OSMHQUKrY_pm9cez94kQ",
- authDomain: "formulario-ebcba.firebaseapp.com",
- projectId: "formulario-ebcba",
- storageBucket: "formulario-ebcba.firebasestorage.app",
- essagingSenderId: "166604505716",
- appId: "1:166604505716:web:b1e7adb7c6c71ec9ca4721"
+  apiKey: "AIzaSyCXQkSjs1E7vA0OSMHQUKrY_pm9cez94kQ",
+  authDomain: "formulario-ebcba.firebaseapp.com",
+  projectId: "formulario-ebcba",
+  storageBucket: "formulario-ebcba.firebasestorage.app",
+  messagingSenderId: "166604505716",
+  appId: "1:166604505716:web:b1e7adb7c6c71ec9ca4721"
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
